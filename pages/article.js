a451303b8ccee6c4300fc26e501c27def01f2e4e
Omar Aguilar <omar.aguilar@wizeline.com>
2022-07-08 10:42:52 -0500

    fix lighthouse

--- pages/article.js
+++ pages/article.js
@@ -9,8 +9,55 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
+        {/* <!-- HTML Meta Tags --> */}
         <title>Core Web Vitals</title>
         <meta name="description" content="Test for core web vitals" />
+
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property="og:url" content="https://next-seo-pi.vercel.app/article/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Core Web Vitals" />
+        <meta property="og:description" content="Test for core web vitals" />
+        <meta property="og:image" content="https://web-dev.imgix.net/image/tcFciHGuF3MxnTr1y5ue01OGLBn2/ZZU8Z7TMKXmzZT2mCjJU.svg" />
+
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="next-seo-pi.vercel.app" />
+        <meta property="twitter:url" content="https://next-seo-pi.vercel.app/article/" />
+        <meta name="twitter:title" content="Core Web Vitals" />
+        <meta name="twitter:description" content="Test for core web vitals" />
+        <meta name="twitter:image" content="https://web-dev.imgix.net/image/tcFciHGuF3MxnTr1y5ue01OGLBn2/ZZU8Z7TMKXmzZT2mCjJU.svg" />
+
+        <script type="application/ld+json" dangerouslySetInnerHTML={{__html: `
+          {
+            "@context": "https://schema.org",
+            "@type": "NewsArticle",
+            "mainEntityOfPage": {
+              "@type": "WebPage",
+              "@id": "https://google.com/article"
+            },
+            "headline": "Core Web Vitals",
+            "image": [
+              "https://example.com/photos/1x1/photo.jpg",
+              "https://example.com/photos/4x3/photo.jpg",
+              "https://example.com/photos/16x9/photo.jpg"
+            ],
+            "datePublished": "2015-02-05T08:00:00+08:00",
+            "dateModified": "2015-02-05T09:20:00+08:00",
+            "author": {
+              "@type": "Person",
+              "name": "John Doe",
+              "url": "http://example.com/profile/johndoe123"
+            },
+            "publisher": {
+              "@type": "Organization",
+              "name": "Google",
+              "logo": {
+                "@type": "ImageObject",
+                "url": "https://google.com/logo.jpg"
+              }
+            }
+          }`}}/>
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>
